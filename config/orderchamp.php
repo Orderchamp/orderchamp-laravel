c39bf080a04481df3de71e4cff78001d919416e6
Dennis Cuijpers <dennis@ifabriek.com>
2019-08-22 20:05:34 +0200

    More env config options

--- config/orderchamp.php
+++ config/orderchamp.php
@@ -5,4 +5,8 @@ return [
     'client_secret' => env('ORDERCHAMP_CLIENT_SECRET'),
     'access_token'  => env('ORDERCHAMP_ACCESS_TOKEN'),
     'shared_secret' => env('ORDERCHAMP_SHARED_SECRET'),
+    'api_url'       => env('ORDERCHAMP_API_URL'),
+    'web_url'       => env('ORDERCHAMP_WEB_URL'),
+    'verify'        => env('ORDERCHAMP_VERIFY'),
+    'timeout'       => env('ORDERCHAMP_TIMEOUT'),
 ];
